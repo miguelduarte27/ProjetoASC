1b90302acc8876d5e896c087ab9fe414cd1c6ad7
tiagoferr25 <50774136+tiagoferr25@users.noreply.github.com>
2024-01-11 22:27:41 +0000

    Inicialização do projeto
    Processos fork()
    Exemplo de Thread

--- Projeto/main.cpp
+++ Projeto/main.cpp
@@ -1,9 +1,41 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
 
-int main()
-{
-	int a = 218291028912;
-
-	printf("%i", a);
-	return 0;
+int main() {
+    pthread_t thread = NULL;
+    if (thread == NULL)
+        printf("thread não criado!\n");
+    return 0;
 }
+
+/*int main() {
+    pid_t pid1, pid2;
+
+    printf("Processo pai (PID=%d)\n", getpid());
+
+    pid1 = fork();
+    if (pid1 == 0) // Processo filho
+    {
+        
+        printf("Processo filho 1 (PID=%d) do pai (PID=%d)\n", getpid(), getppid());
+    }
+    else if (pid1 > 0)// Processo pai 
+    {
+        pid2 = fork(); // Processo 2
+        if (pid2 == 0) // Processo filho do primeiro filho
+        {
+            printf("Processo filho 2 (PID=%d) do filho 1 (PID=%d)\n", getpid(), getppid());
+        }
+        else if (pid2 > 0) // Pai do segundo filho
+        {  
+            wait(NULL); 
+            printf("Processo pai do segundo filho (PID=%d)\n", getpid());
+        }
+        wait(NULL);
+    }
+
+    return 0;
+}*/
